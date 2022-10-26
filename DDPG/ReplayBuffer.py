5ed035850eb6b72cda46a0d604216713e7fc184b
reecehuff <reecedhuff@gmail.com>
2022-10-25 17:02:29 -0700

    Switched order of tuples

--- DDPG/ReplayBuffer.py
+++ DDPG/ReplayBuffer.py
@@ -6,28 +6,28 @@ class Memory:
     def __init__(self, max_size):
         self.buffer = deque(maxlen=max_size)
     
-    def push(self, state, action, reward, next_state, done):
-        experience = (state, action, np.array([reward]), next_state, done)
+    def push(self, state, next_state, action, reward, done):
+        experience = (state, next_state, action, np.array([reward]), done)
         self.buffer.append(experience)
 
     def sample(self, batch_size):
         state_batch = []
+        next_state_batch = []
         action_batch = []
         reward_batch = []
-        next_state_batch = []
         done_batch = []
 
         batch = random.sample(self.buffer, batch_size)
 
         for experience in batch:
-            state, action, reward, next_state, done = experience
+            state, next_state, action, reward, done = experience
             state_batch.append(state)
+            next_state_batch.append(next_state)
             action_batch.append(action)
             reward_batch.append(reward)
-            next_state_batch.append(next_state)
             done_batch.append(done)
         
-        return state_batch, action_batch, reward_batch, next_state_batch, done_batch
+        return state_batch, next_state_batch, action_batch, reward_batch, done_batch
 
     def __len__(self):
         return len(self.buffer)
